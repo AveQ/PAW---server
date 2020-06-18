d3f3362d66ff01c46ada3bb0d780fc463c7d0e54
Marcin Pietras <martinpie10@gmail.com>
2020-06-18 15:39:39 +0200

    add ssl and finish project

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-const http = require('http');
-const app = require('./app')
+const https = require('https');
+const app = require('./app');
+const fs = require('fs');
 const port = process.env.PORT || 3001;
 
-const server = http.createServer(app);
+const options = {
+    key: fs.readFileSync(__dirname + '/ssl/key.pem'),
+    cert: fs.readFileSync(__dirname + '/ssl/cert.pem'),
+};
+
+const server = https.createServer(options, app);
 
 server.listen(port);
